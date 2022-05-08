cdfb3ef8d65741355121020b6a06dac9b075ff74
sadikhossain69 <sadikhossain433@gmail.com>
2022-05-08 17:51:12 +0600

    jwt token accessed

--- index.js
+++ index.js
@@ -4,12 +4,30 @@ const cors = require('cors');
 const req = require('express/lib/request');
 const res = require('express/lib/response');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const jwt = require('jsonwebtoken');
+// const { decode } = require('jsonwebtoken');
 require('dotenv').config()
 const port = process.env.PORT || 5000
 
 // Middleware
 app.use(cors())
 app.use(express.json())
+
+function verifyJWT(req, res, next) {
+    const authHeader = req.headers.authorization
+    if (!authHeader) {
+        return res.status(401).send({ massage: 'unauthorized access' })
+    }
+    const token = authHeader.split(' ')[1]
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+            return res.status(403).send({ massage: 'forbidded access' })
+        }
+        console.log('decoded', decoded);
+        req.decoded = decoded
+        next()
+    })
+}
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@dreamelectronics.eefg6.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
@@ -23,7 +41,7 @@ async function run() {
         console.log("db connected");
 
         // Inventory Get API
-        app.get('/inventories', async(req, res) => {
+        app.get('/inventories', async (req, res) => {
             const query = {}
             const cursor = inventoryCollection.find(query)
             const result = await cursor.toArray()
@@ -38,12 +56,29 @@ async function run() {
         })
 
         // Added Items Get API
-        app.get('/addedItems', async (req, res) => {
-            const query = { }
-            console.log(query);
-            const cursor = addedCollection.find(query)
-            const result = await cursor.toArray()
-            res.send(result)
+        app.get('/addedItems', verifyJWT, async (req, res) => {
+            const decodedEmail = req.decoded?.email
+            const email = req.query?.email
+            console.log(email);
+            if (email === decodedEmail) {
+                const query = { email: email }
+                console.log(query);
+                const cursor = addedCollection.find(query)
+                const result = await cursor.toArray()
+                res.send(result)
+            }
+            else {
+                res.status(403).send({ massage: 'forbidder access' })
+            }
+        })
+
+        // AUTH
+        app.post('/login', async (req, res) => {
+            const user = req.body
+            const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+                expiresIn: '1d'
+            })
+            res.send({ accessToken })
         })
 
         // Added Item Delete API
