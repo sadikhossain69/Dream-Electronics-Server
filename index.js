12e53879ad516954fd1550f979babec37e0d4c26
sadikhossain69 <sadikhossain433@gmail.com>
2022-05-08 12:43:34 +0600

    get and delete addItems api created

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 const cors = require('cors');
 const req = require('express/lib/request');
 const res = require('express/lib/response');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const port = process.env.PORT || 5000
 
@@ -36,6 +36,23 @@ async function run() {
             const result = await addedCollection.insertOne(addedItem)
             res.send(result)
         })
+
+        // Added Items Get API
+        app.get('/addedItems', async (req, res) => {
+            const query = { }
+            console.log(query);
+            const cursor = addedCollection.find(query)
+            const result = await cursor.toArray()
+            res.send(result)
+        })
+
+        // Added Item Delete API
+        app.delete('/addedItems/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await addedCollection.deleteOne(query);
+            res.send(result);
+        });
     }
     finally {
         // client.close()
